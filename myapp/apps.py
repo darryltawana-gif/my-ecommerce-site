efc021f74f1acd1d0cd68fda7386547a314c8f9f
darryltawana-gif <darryltawana@gmail.com>
2026-07-24 00:30:36 -0700

    Auto create superuser on deploy

--- myapp/apps.py
+++ myapp/apps.py
@@ -2,4 +2,15 @@ from django.apps import AppConfig
 
 
 class MyappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                'admin', 'darryltawana@gmail.com','AdminPassword123!'
+            )
+
