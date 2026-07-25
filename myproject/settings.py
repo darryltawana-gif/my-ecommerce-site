6f4f0a62fbb68393d25c9e163692df59c9652da1
darryltawana-gif <darryltawana@gmail.com>
2026-07-25 13:02:52 -0700

    Added media settings and media URL routes

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +133,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
